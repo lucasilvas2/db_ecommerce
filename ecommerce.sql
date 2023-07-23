ecca38bb31343eb247ddb2434e132d3778539df5
Lucas <lucasoliver1068@gmail.com>
2023-07-23 14:10:23 -0300

    feat: initial repository

---
+++ ecommerce.sql
@@ -0,0 +1,15 @@
+-- criar db
+CREATE DATABASE ecommerce;
+USE ecommerce;
+
+-- criar table cliente
+CREATE TABLE client
+(
+    idClient int
+    auto_increment primary key,
+ 
+);
+-- criar table cliente
+-- criar table cliente
+
+
